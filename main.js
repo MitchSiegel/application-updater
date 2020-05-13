b85011e3efd233cc737882539394acd266fc696e
Mitch Siegel <mitchsiegl14@gmail>
2020-05-12 18:25:12 -0600

    Basic HTML theme

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+require('electron-reload')(__dirname);
 
 function createWindow () {
   // Create the browser window.
+
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
+  mainWindow.webContents.openDevTools()
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
